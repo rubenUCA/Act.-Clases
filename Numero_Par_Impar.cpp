339f346a315d457a2f663eba57ab03df3828f74d
rubenUCA <00076325@uca.edu.sv>
2025-04-29 09:00:16 -0600

    Añadi ejercicio de Numero par e impar a repositorio

---
+++ Numero_Par_Impar.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numero;
+    cout << "Ingrese un numero: ";
+    cin >> numero;
+
+    if (numero % 2 == 0) {
+        cout << "El numero es par." << endl;
+    } else {
+        cout << "El numero es impar." << endl;
+    }
+
+    return 0;
+}   
